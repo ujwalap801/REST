2a7b7882b2815f6569da1c8ad1882f428138669b
ujwala <ujwalap801@gmail.com>
2024-09-26 13:58:05 +0530

    delete nodemodules

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require("path");
 const port =8080;
 const {v4:uuidv4} = require("uuid");
+const methodOverride=require("method-override");
 
-app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
  
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -64,6 +66,37 @@ app.get("/posts/:id",(req,res)=>
     // console.log(post);
     // res.send("request is working");
     res.render("show",{post});
+})
+
+
+// TO UPDATE CONTENT
+app.patch("/posts/:id",(req,res)=>
+{
+    let {id}= req.params;
+    // console.log(id);
+    let newcontent = req.body.content;
+    let post = posts.find((p)=>id === p.id)
+    post.content = newcontent;
+    // console.log(post);
+    res.redirect("/posts");
+})
+
+// EDIT THE CONTENT
+app.get("/posts/:id/edit",(req,res)=>
+{
+    let {id}= req.params;
+    let post = posts.find((p)=>id === p.id)
+    res.render("edit",{post});
+})
+
+
+// DELETE POST
+app.delete("/posts/:id",(req,res)=>
+{
+    let {id}= req.params;
+     posts = posts.filter((p)=>id !== p.id);  //we use filter method
+    // res.send("delete sucess");
+    res.redirect("/posts");
 })
 
 app.listen(port,()=>
